8566ff7b2ff7e7c0e42b7c2ce5f1c296237c7f9e
Yakufaith <jaynjaycob@gmail.com>
2021-09-08 00:15:21 +0200

    Advanced concepts on slicing and concatenating

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print("""{:s} {:s} {:s}""".format( str[39:66], str[107:111],
-                                   str[0:6])
-)
+print("{:s}".format(str[39:67]), end="")
+print("{:s}".format(str[107:112]), end = "")
+print("{:s}".format(str[0:6]))
