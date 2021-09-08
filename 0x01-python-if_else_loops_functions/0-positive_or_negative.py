1e08ce4c178cda9105d85d6010a693c3d5764ca5
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 01:26:01 +0200

    Program determines whether a number is positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,9 +4,12 @@ import random
 
 number = random.randint(-10, 10)
 
-# If number is greater than or equal to 0 print its positive
-# Otherwise if number is less than 0 print its negative
-if number >= 0:
+# if number is greater than or 0 print its positive
+if number > 0:
     print("{} is positive".format(number))
-else:
+# however, if number is less than 0 print its negative
+elif number < 0:
     print("{} is negative".format(number))
+# otherwise, if number is 0 print its zero
+else:
+    print("{} is zero".format(number))
