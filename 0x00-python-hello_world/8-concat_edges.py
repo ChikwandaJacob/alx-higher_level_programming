6b998f00221e78a490e23a0d386c0648a208015c
Yakufaith <jaynjaycob@gmail.com>
2021-09-08 09:51:57 +0200

    Advanced concepts of slicing and formatting

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print("{:s}".format(str[39:67]), end="")
-print("{:s}".format(str[107:112]), end="")
-print("{:s}".format(str[0:6]))
+language that combines remarkable power with very clear syntax"
+
+print("{} ".format(str[39:66]), end="")
+print("{}".format(str[106:111]), end="")
+print("{}".format(str[0:6]))
