996a4063a08ec6d3a54846ee6cacbf6a7ff0bd4b
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 01:42:53 +0200

    Prints the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -19,7 +19,7 @@ if last_digit > 5:
     text = "and is greater than 5"
 elif last_digit < 6 and last_digit != 0:
     text = "and is less than 6 and not 0"
-else:
+elif last_digit == 0:
     text = "and is 0"
 
 # Print last digit
