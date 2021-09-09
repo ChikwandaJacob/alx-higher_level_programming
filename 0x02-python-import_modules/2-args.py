f2a6dc888482b706fc9c69737ff10f02e57f197c
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 22:45:47 +0200

    Program displays command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+-------------------
+Program Description
+-------------------
+prints the number of and the list of its arguments.
+"""
+
+""" Ensure code won't be executed when imported elsewhere """
+if __name__ == "__main__":
+
+    """ Import sys module so as to access command line arguments """
+    import sys
+
+    """ Retrieve all command line arguments and the length of the list """
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    """ Print outcome based on given predicate """
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, argv[1]))
+    else:
+        print("{} arguments:".format(argc))
+        for index in range(1, argc):
+            print("{}: {}".format(index, argv[index]))
