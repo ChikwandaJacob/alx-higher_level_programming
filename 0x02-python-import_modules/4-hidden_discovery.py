5b191827c0cd05e145fc95e4697d593d7f838d2e
Yakufaith <jaynjaycob@gmail.com>
2021-09-10 00:19:26 +0200

    Program prints the content of a precomipled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -10,17 +10,17 @@ prints all the names defined by the compiled module hidden_4.pyc
 if __name__ == "__main__":
 
     """ Import the module in the program description """
-    import hidden_4
+    import hidden_4 as hidden
 
     """ Save contents of module in list and sort them """
-    module_content = dir(hidden_4)
+    module_content = dir(hidden)
     module_content.sort()
 
     """ Define some helper variables """
     module_length = len(module_content)
     start_symbol = "__"
 
-    """ Print Content of the list with exception of names that start with`__`"""
+    """ Print Content of the list, except names that start with `__` """
     for index in range(0, module_length):
         text = module_content[index]
 
