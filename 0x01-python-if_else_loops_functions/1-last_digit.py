a1702b451598d3d74284955b02842183682cf886
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 02:36:21 +0200

    Prints last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,23 +4,18 @@ import random
 
 number = random.randint(-10000, 10000)
 
-# Convert random generated number into string
 text = str(number)
 
-# Store last digit
 last_digit = int(text[len(text) - 1])
 
-# Ensure last digit has same sign as origial number
 if number < 0:
     last_digit *= -1
 
-# Do some checks on the last digit
 if last_digit > 5:
     text = "and is greater than 5"
 elif last_digit < 6 and last_digit != 0:
     text = "and is less than 6 and not 0"
-elif last_digit == 0:
+else:
     text = "and is 0"
 
-# Print last digit
 print("Last digit of {:d} is {:d} {:s}".format(number, last_digit, text))
