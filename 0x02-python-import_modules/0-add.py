a59842275fe64a5f687d9bb532e2a51fc425d43c
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 15:30:27 +0200

    Program finds the sum of two numbers.

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# import the module named add_0
+from add_0 import add
+
+# Define to variables a and b
+a = 1
+b = 2
+
+# involke the function call add() to print sum of 1 and 2
+print("{} + {} = {}".format(a, b, add(1, 2)))
