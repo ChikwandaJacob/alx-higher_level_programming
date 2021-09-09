8dfb95b3d9974c23e4dd69b0663306323c06f05e
Yakufaith <jaynjaycob@gmail.com>
2021-09-10 00:16:19 +0200

    Program prints the content of a precomipled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -10,10 +10,10 @@ prints all the names defined by the compiled module hidden_4.pyc
 if __name__ == "__main__":
 
     """ Import the module in the program description """
-    import hidden_4 as hidden
+    import hidden_4
 
     """ Save contents of module in list and sort them """
-    module_content = dir(hidden)
+    module_content = dir(hidden_4)
     module_content.sort()
 
     """ Define some helper variables """
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for index in range(0, module_length):
         text = module_content[index]
 
-        if start_symbol == text[0:2]:
+        if start_symbol == text[:2]:
             continue
         else:
             print(text)
