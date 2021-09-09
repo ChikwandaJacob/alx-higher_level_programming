d2bde21fa8d312a97cea37a0056430af6b3b0263
Yakufaith <jaynjaycob@gmail.com>
2021-09-09 20:08:44 +0200

    Program finds the sum of two numbers.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-# involke the function call add() to print sum of 1 and 2
+''' involke the function call add() to print sum of 1 and 2 '''
 if __name__ == "__main__":
 
-    # import the module named add_0
+    ''' import the module named add_0 '''
     from add_0 import add
 
-    # Define to variables a and b
+    ''' Define to variables a and b '''
     a = 1
     b = 2
 
