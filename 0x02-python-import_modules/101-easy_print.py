d0db01d9ca76f1b85532739600725d828e417b41
Yakufaith <jaynjaycob@gmail.com>
2021-09-10 02:37:12 +0200

    Print method changed

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,16 +1,2 @@
 #!/usr/bin/python3
-
-"""
--------------------
-Program Description
--------------------
-Prints #pythoniscool, followed by a new line, in the standard output
-
-----------
-Conditions
-----------
-1. Program should be maximum 2 lines long
-2. Use of print or eval or open or import sys is not allowed
-"""
-
 input("#pythoniscool")
