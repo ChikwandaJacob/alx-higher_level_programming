9b6c7fdd081aef58fdd9229dbeec926d50365dff
Yakufaith <jaynjaycob@gmail.com>
2021-09-10 02:34:44 +0200

    Program prints the alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -7,6 +7,5 @@ Program Description
 prints the alphabet in uppercase, followed by a new line.
 """
 
-for index in range(65, 90):
-    print("{}".format(chr(index)), end="")
-print("")
+import string
+print(string.ascii_uppercase)
