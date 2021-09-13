1d862ae052d6085fbacfe1cb3d95a6af9b2c6acb
Yakufaith <jaynjaycob@gmail.com>
2021-09-13 09:06:03 +0200

    File contains a function to print a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+-------------------
+Program Description
+-------------------
+
+This file contains a function that prints all integers of a list. 
+"""
+
+def print_list_integer(my_list=[]):
+
+    """ Loop through the provided list """
+    for item in my_list:
+        print("{}".format(item))
