7c376e0f7863cd89215fafa739ced2052f4c1b3e
Yakufaith <jaynjaycob@gmail.com>
2021-09-15 23:07:45 +0200

    contains a module that returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    ------------
+    About Module
+    ------------
+    returns the number of keys in a dictionary.
+
+    ---------
+    More Info
+    ---------
+    takes a dictionary
+"""
+
+
+def number_keys(a_dictionary):
+
+    return (len(a_dictionary.keys()))
