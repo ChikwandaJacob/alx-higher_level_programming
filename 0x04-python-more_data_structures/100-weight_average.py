6662f8c9a1408c98601b9808ed306e41080938ac
Yakufaith <jaynjaycob@gmail.com>
2021-09-16 01:23:48 +0200

    Module Calculates average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+
+    """ Get total weights """
+    weight_by_score = 0
+    cum_weight = 0
+
+    for item in my_list:
+
+        weight_by_score += (item[0] * item[1])
+        cum_weight += item[1]
+
+    return (weight_by_score / cum_weight)
