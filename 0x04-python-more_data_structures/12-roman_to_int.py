fd94f4dcb9f2197069f0f5203eb4a5f24da13ca0
Yakufaith <jaynjaycob@gmail.com>
2021-09-16 01:12:37 +0200

    module coverts roman numerals to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,27 +16,33 @@
 def roman_to_int(roman_string):
 
     """ Check if string is valid """
-    if type(roman_string) != str:
+    if type(roman_string) == str or type(roman_string) is None:
+
+        """ Split the string into parts """
+        rom_str = list(roman_string)
+
+        """ Define a dictionary of roman values """
+        rom_num = {
+            "I": 1, "V": 5, "X": 10,
+            "L": 50, "C": 100, "D": 500,
+            "M": 1000
+        }
+
+        """ Get keys """
+        symbols = list(rom_num.keys())
+
+        """ Loop through """
+        result = 0
+
+        for sym in rom_str:
+
+            if sym in symbols:
+                result += rom_num[sym]
+            else:
+                return (0)
+
+        """ Return the result """
+        return (result)
+
+    else:
         return (0)
-
-    """ Split the string into parts """
-    rom_str = list(roman_string)
-
-    """ Define a dictionary of roman values """
-    rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
-    """ Get keys """
-    symbols = list(rom_num.keys())
-
-    """ Loop through """
-    result = 0
-
-    for sym in rom_str:
-
-        if sym in symbols:
-            result += rom_num[sym]
-        else:
-            return (0)
-
-    """ Return the result """
-    return (result)
