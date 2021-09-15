382046ba70c5857c2fe2f33e587e638de6915044
Yakufaith <jaynjaycob@gmail.com>
2021-09-15 23:02:52 +0200

    contains module to print non intersection elements of two sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    ------------
+    About Module
+    ------------
+    returns a set of all elements present in only one set.
+
+    ---------
+    More Info
+    ---------
+    takes two sets
+"""
+
+
+def only_diff_elements(set_1, set_2):
+
+    return (set_1 ^ set_2)
