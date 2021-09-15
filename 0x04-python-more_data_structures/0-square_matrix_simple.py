4f584abcc99ed01cd0fcee60c842ddc93cfdf584
Yakufaith <jaynjaycob@gmail.com>
2021-09-15 22:16:21 +0200

    Contains a module that doubles the contents of a list of lists

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    ------------
+    About Module
+    ------------
+    computes the square value of all integers of a matrix
+
+    ---------
+    More Info
+    ---------
+    @matrix: this is a list of lists
+"""
+
+
+def square_matrix_simple(matrix=[]):
+
+    """ Create an empty list """
+    dbl_matrix = []
+
+    """ Double the contents of matrix and save them in dbl_matrix """
+    for num in matrix:
+        dbl_matrix.append(list(map((lambda elem : elem ** 2), num)))
+
+    """ Return the new doubled list """
+    return (dbl_matrix)
