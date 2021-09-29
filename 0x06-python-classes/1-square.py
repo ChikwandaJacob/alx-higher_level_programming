ac0210ba719e5b8790d3cce9400b52397a79432b
Yakufaith <jaynjaycob@gmail.com>
2021-09-29 02:28:35 +0200

    Defines square class based on 0-sqaure.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+   This class defines a sqaure
+"""
+
+
+class Square:
+
+    """
+       This class contains no class attributes
+    """
+
+    def __init__(self, size):
+
+        """
+           The init method contains one private instance attribute size
+        """
+        self.__size = size
