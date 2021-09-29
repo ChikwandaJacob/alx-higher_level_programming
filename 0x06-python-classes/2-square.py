798fe71bcc9052d8c6ceb0f6a452c9ee8097e059
Yakufaith <jaynjaycob@gmail.com>
2021-09-29 02:38:32 +0200

    Defines square class. Builds on 1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+   This class defines a sqaure
+"""
+
+
+class Square:
+
+    """
+       This class contains no class attributes
+    """
+
+    def __init__(self, size=0):
+
+        """
+           The init method contains one private instance attribute size
+
+           __init__ raises two exceptions:
+           1. TypeError when size is not an integer
+           2. ValueError when size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
