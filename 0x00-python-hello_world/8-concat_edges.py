24f0cce4d40919ae16601b02d344d65974076bc7
Yakufaith <jaynjaycob@gmail.com>
2021-11-19 14:16:04 +0200

    Program 9 updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-print("{} ".format(str[39:66]), end="")
-print("{}{}".format(str[106:111], str[0:6]))
+
+print("{:s} {:s} {:s}".format(str[39:66], str[106:110], str[0:6]))
