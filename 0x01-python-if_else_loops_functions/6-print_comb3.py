b9338115f3976fa531b6a3ebca924dab98be7aa5
Yakufaith <jaynjaycob@gmail.com>
2021-11-19 16:35:46 +0200

    Program 7 updated

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for i in range(0, 9):
+    for j in range(i + 0, 10):
+
+        if (i < 8):
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            if (j < 9):
+                print("{:d}{:d}".format(i, j), end=", ")
+            else:
+                print("{:d}{:d}".format(i, j))
