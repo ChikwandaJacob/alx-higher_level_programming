701be523afbd3985e552f3829ec5efbda7288c08
Yakufaith <jaynjaycob@gmail.com>
2021-11-19 16:11:53 +0200

    Program 6 updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,7 @@
 for num in range(0, 100):
 
     if num < 99:
-        print("{:d}".format(num), end=", ")
+        print("{:s}".format(
+            "0" + str(num) if num <= 9 else str(num)), end=", ")
     else:
         print("{:d}".format(num))
